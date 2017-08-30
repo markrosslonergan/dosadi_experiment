62d69871063fae54b7ce9a2cc3ae19a7e71045af
Mark Ross-Lonergan <markrosslonergan@gmail.com>
2017-08-30 14:13:42 -0500

    Somewhat working, and definitely compiling

---
+++ covar_splines/runall_covar2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+while read p; do
+  mkdir -p data/$p
+  sed "s/XXX/$p/g" build_uboone_covar_template.xml.xml > data/$p/build_uboone_covar_template_$p.xml
+  cp build_uboone_covar data/$p/
+  cd data/$p/
+  ./build_uboone_covar --xml build_uboone_covar_template_$p.xml > log.log
+  cd ../../
+done < syst2.txt
